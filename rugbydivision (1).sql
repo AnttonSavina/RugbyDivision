d4f959281b90c10c928b7613f8a81b48082445e4
AnttonSavina <160036866+AnttonSavina@users.noreply.github.com>
2026-05-05 13:41:12 +0200

    ajout sql

---
+++ rugbydivision (1).sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3366
+-- Généré le : mar. 05 mai 2026 à 13:40
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `rugbydivision`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `arbitre`
+--
+-- Erreur de lecture de structure pour la table rugbydivision.arbitre : #1932 - Table &#039;rugbydivision.arbitre&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table rugbydivision.arbitre : #1064 - Erreur de syntaxe près de &#039;FROM `rugbydivision`.`arbitre`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `club`
+--
+-- Erreur de lecture de structure pour la table rugbydivision.club : #1932 - Table &#039;rugbydivision.club&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table rugbydivision.club : #1064 - Erreur de syntaxe près de &#039;FROM `rugbydivision`.`club`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `faitobjetdetransfert`
+--
+-- Erreur de lecture de structure pour la table rugbydivision.faitobjetdetransfert : #1932 - Table &#039;rugbydivision.faitobjetdetransfert&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table rugbydivision.faitobjetdetransfert : #1064 - Erreur de syntaxe près de &#039;FROM `rugbydivision`.`faitobjetdetransfert`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `joueur`
+--
+-- Erreur de lecture de structure pour la table rugbydivision.joueur : #1932 - Table &#039;rugbydivision.joueur&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table rugbydivision.joueur : #1064 - Erreur de syntaxe près de &#039;FROM `rugbydivision`.`joueur`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `matchrugby`
+--
+-- Erreur de lecture de structure pour la table rugbydivision.matchrugby : #1932 - Table &#039;rugbydivision.matchrugby&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table rugbydivision.matchrugby : #1064 - Erreur de syntaxe près de &#039;FROM `rugbydivision`.`matchrugby`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `oppose`
+--
+-- Erreur de lecture de structure pour la table rugbydivision.oppose : #1932 - Table &#039;rugbydivision.oppose&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table rugbydivision.oppose : #1064 - Erreur de syntaxe près de &#039;FROM `rugbydivision`.`oppose`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit_stat`
+--
+-- Erreur de lecture de structure pour la table rugbydivision.produit_stat : #1932 - Table &#039;rugbydivision.produit_stat&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table rugbydivision.produit_stat : #1064 - Erreur de syntaxe près de &#039;FROM `rugbydivision`.`produit_stat`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `saison`
+--
+-- Erreur de lecture de structure pour la table rugbydivision.saison : #1932 - Table &#039;rugbydivision.saison&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table rugbydivision.saison : #1064 - Erreur de syntaxe près de &#039;FROM `rugbydivision`.`saison`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stade`
+--
+-- Erreur de lecture de structure pour la table rugbydivision.stade : #1932 - Table &#039;rugbydivision.stade&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table rugbydivision.stade : #1064 - Erreur de syntaxe près de &#039;FROM `rugbydivision`.`stade`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `staff`
+--
+-- Erreur de lecture de structure pour la table rugbydivision.staff : #1932 - Table &#039;rugbydivision.staff&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table rugbydivision.staff : #1064 - Erreur de syntaxe près de &#039;FROM `rugbydivision`.`staff`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+-- Erreur de lecture de structure pour la table rugbydivision.utilisateur : #1932 - Table &#039;rugbydivision.utilisateur&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table rugbydivision.utilisateur : #1064 - Erreur de syntaxe près de &#039;FROM `rugbydivision`.`utilisateur`&#039; à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
